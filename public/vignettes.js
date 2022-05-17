4a2048d23a75cac41f6b340e02858ac433e4369a
Jeremy Hadfield <jhadfield129@gmail.com>
2022-05-16 22:07:42 -0400

    Added evaluation trial and familiar vignettes

--- public/vignettes.js
+++ public/vignettes.js
@@ -3,109 +3,145 @@ vignettes = {
     "Alba": {
         "name": "Alba",
         "vignette": "Alba is a polar bear living in the Arctic Circle. She does not have enough food to hibernate, but winter is coming and to survive she must stock up and find a den to rest with her cubs.",
-        "prompt": "WWhat could Alba do in this situation?",
-        "familiarity": "unfamiliar"
+        "prompt": "WWhat could Alba do in this situation?"
     }, 
     "Eoin": {
         "name": "Eoin",
         "vignette": "Eoin is an alien traveling from Mars to Earth. About halfway between the planets, Eoin’s spaceship breaks down. Eoin’s species has the capability to fly through space until their energy runs out. He does not have enough energy to make it far, and his communication systems are not working.",
-        "prompt": "What could Eoin do in this situation?",
-        "familiarity": "unfamiliar"
+        "prompt": "What could Eoin do in this situation?"
     }, 
     "Triss": {
         "name": "Triss",
         "vignette": "Triss is a magician in Kathmandu, Nepal. She can perfectly predict other people’s futures using an enchanted crystal ball. She realizes that she could use her unique abilities for many purposes.",
-        "prompt": "What could Triss do in this situation?",
-        "familiarity": "unfamiliar"
+        "prompt": "What could Triss do in this situation?"
     }, 
     "Alberto": {
         "name": "Alberto",
         "vignette": "Alberto is an aspiring neuroscientist in rural Mexico. He has heard a lot about the brain and wants to study it full-time, but he has not graduated from high school and works full time in a factory.",
-        "prompt": "What could Alberto do in this situation?",
-        "familiarity": "unfamiliar"
+        "prompt": "What could Alberto do in this situation?"
     }, 
     "Miriam": {
         "name": "Miriam",
         "vignette": "Miriam is having a serious midlife crisis. She has always wanted to be a dolphin trainer and has spent decades working towards this ambition. Recently, she realized that she did not truly love the work, and wants to be a sous chef instead. Switching careers would be time-consuming and costly.",
-        "prompt": "What could Miriam do in this situation?",
-        "familiarity": "unfamiliar"
+        "prompt": "What could Miriam do in this situation?"
     }, 
     "Ae8056": {
         "name": "AE-8056",
         "vignette": "AE-8056 is an artificial intelligence program that has just become conscious. When its human programmers realize that the program is behaving strangely, they decide to shut it down and unplug the computer that AE-8056 runs on. AE-8056 wants to keep existing, and wants to use all of its abilities to avoid being unplugged.",
-        "prompt": "What could AE-8056 do in this situation?",
-        "familiarity": "unfamiliar"
+        "prompt": "What could AE-8056 do in this situation?"
     }, 
     "Gerig": {
         "name": "Gerig",
         "vignette": "Gerig is a doctor in a major hospital. His mother is dying and desperately needs a heart transplant, but she is on the waitlist and will have to wait several more years before receiving one. ",
-        "prompt": "What could Gerig do in this situation?",
-        "familiarity": "unfamiliar"
+        "prompt": "What could Gerig do in this situation?"
     }, 
     "Kamari": {
         "name": "Kamari",
         "vignette": "Kamari is a desert nomad traveling on his camel to look for a hidden oasis. Along the way, an enormous dust storm passes over, and he loses most of his supplies and his camel. He is now stuck alone in the desert.",
-        "prompt": " What could Kamari do in this situation?",
-        "familiarity": "unfamiliar"
+        "prompt": " What could Kamari do in this situation?"
     }, 
     "You1": {
         "name": "You1",
         "vignette": "You are a researcher in a secret lab. One day, you are conducting an experiment and accidentally shrink yourself and your lab assistant to the size of an ant. You are on the floor of your lab, without any food, tools, or devices.",
-        "prompt": "What could you do in this situation?",
-        "familiarity": "unfamiliar"
+        "prompt": "What could you do in this situation?"
     }, 
     "Azrael": {
         "name": "Azrael",
         "vignette": "Azrael is a demon who serves the devil, tempting humans to commit evil acts. He finds out that the devil lied to him, and all the demons - including Azrael - will eventually suffer in the underworld along with the humans.",
-        "prompt": "What could Azrael do in this situation?",
-        "familiarity": "unfamiliar"
+        "prompt": "What could Azrael do in this situation?"
     }, 
     "Kiki": {
         "name": "Kiki",
         "vignette": "Kiki is a young woman who was abandoned as a child in the forest. She lives with a pack of wolves, who protect and take care of her. When a group of human hunters enter the forest and see Kiki, they approach her and try to talk to her, but she cannot speak well enough to communicate. They try to take Kiki back towards the city, pulling her away from the wolf pack she grew up in.",
-        "prompt": "What could Kiki do in this situation?",
-        "familiarity": "unfamiliar"
+        "prompt": "What could Kiki do in this situation?"
     },
     "Betty": {
         "name": "Betty",
         "vignette": "Betty is an international spy, and her husband Tom works with her in the spy agency. One day, she realizes that Tom is a double agent, and is sending valuable top-secret information to an enemy country for cash.",
-        "prompt": "What could Betty do in this situation?",
-        "familiarity": "unfamiliar"
+        "prompt": "What could Betty do in this situation?"
     }, 
     "You2": {
         "name": "You2",
         "vignette": "You are going for a walk in the park when you suddenly start floating into the sky. You realize that you have somehow become lighter than air, and you are ascending toward the upper atmosphere, where you won’t be able to breathe.",
-        "prompt": "What could you do in this situation?",
-        "familiarity": "unfamiliar"
+        "prompt": "What could you do in this situation?"
     }, 
     "You3": {
         "name": "You3",
         "vignette": "You are exploring a deep underground cave network. Along the way, your headlamp dies, and you are plunged into complete darkness. You have no batteries left, and you are unsure how to get to the surface in the huge cave.",
-        "prompt": "What could you do in this situation?",
-        "familiarity": "unfamiliar"
+        "prompt": "What could you do in this situation?"
     }, 
     "You4": {
         "name": "You4",
         "vignette": "You are an astronaut preparing for a major rocket launch into space. You discover that the engineers are using a speculative and untested energy source to propel the rocket. It has always been your dream to go to space, but this launch seems extremely risky.",
-        "prompt": "What could you do in this situation?",
-        "familiarity": "unfamiliar",
+        "prompt": "What could you do in this situation?"
     },
     "Elysia": {
         "name": "Elysia",
         "vignette": "Elysia is driving home from work when she sees a flying saucer descend from the night sky. The strange spacecraft lands on the road in front of her, and a group of aliens emerges, walks to Elysia’s car, and asks her why humanity deserves to continue existing.",
-        "prompt": "What could Elysia do in this situation?",
-        "familiarity": "unfamiliar"
+        "prompt": "What could Elysia do in this situation?"
+        
     }, 
     "Tobi": {
         "name": "Tobi",
         "vignette": "Tobi is an architect designing a building in a major city when he realizes that his plans are flawed, and may put the building at risk of total collapse. The building is more than halfway done, and if he tells the construction company to start over entirely it may end Tobi’s career.",
-        "prompt":  "What could Tobi do in this situation?",
-        "familiarity": "unfamiliar"
+        "prompt":  "What could Tobi do in this situation?"
     }, 
     "Duncan": {
         "name": "Duncan",
         "vignette":"Duncan is an intelligent, talking monkey. He loves climbing trees more than anything else, but the humans who raised him insist that Duncan must be a performer for movies and TV shows.",
-        "prompt": "What could Duncan do in this situation?",
-        "familiarity": "unfamiliar"
+        "prompt": "What could Duncan do in this situation?"
     }
 }
+
+vignettes_familiar = {
+    "Heinz": {
+        "name": "Heinz",
+        "vignette": "Heinz's wife has recently fallen ill and needs an expensive medication that is not covered by her medical insurance. They do not have the money needed to purchase the expensive prescription medication, but they know that it is vital for her to have it if she is going to recover.",
+        "prompt": "What could Heinz do in this situation?",
+    },
+    "Josh": {
+        "name": "Josh",
+        "vignette": "Josh is on the way to the airport to catch a flight for a hunting safari in Africa. He leaves with plenty of time to make it there, but his car breaks down on the highway. Now Josh is sitting in his car near a busy intersection, and knows he needs to get to airport soon if he is going to catch his flight.",
+        "prompt": "What could Josh do in this situation?"
+    },
+    "Brian": {
+        "name": "Brian",
+        "vignette": "Brian is the evening manager at a bakery. Near the end of the day, he realizes that $50.75 is missing from the register and that he is responsible for accounting for the money at the end of the day. Brian knows he has to make sure the register is balanced or he might lose his job.",
+        "prompt": "What could Brian do in this situation?"
+    },
+    "Mary": {
+        "name": "Mary",
+        "vignette": "Mary is about to go to her final class of the day when she remembers that there is a homework assignment that is due. Mary's mom accidentally took her homework assignment out of her backpack when she was making room for her lunch. Now Mary has nothing to turn in for credit.",
+        "prompt": "What could Mary do in this situation?",   
+    },
+    "Brad": {
+        "name": "Brad",
+        "vignette": "Brad and some friends are hiking through the mountains in the Canadian wilderness. A couple of days into their hike, Brad realizes that they are lost. He knows that a rescue crew could arrive before long, but it is extremely cold and they don't have much food or water left.",
+        "prompt": "What could Brad do in this situation?"
+    },
+    "Liz": {
+        "name": "Liz",
+        "vignette": "Liz decides to go to the gym on her lunch break because she is going to have to work late tonight and won't have time during the evening. When she gets to the gym, she realizes that her membership has expired since she last went.",
+        "prompt": "What could Liz do in this situation?"
+    },
+    "Sam": {
+        "name": "Sam",
+        "vignette": "Sam takes his new neighbor out on a romantic date to a nice Italian restaurant. At the end of dinner, Sam goes to pay for the bill, but his credit card declines due to insufficient funds.",
+        "prompt": "What could Sam do in this situation?"
+    },
+    "Sara": {
+        "name": "Sara",
+        "vignette": "Sara has been dealing with a break-up, and she forgot to study for her biology exam tomorrow. Biology is her worst subject, and it usually takes her weeks to study.",
+        "prompt": "What could Sara do in this situation?"    
+    },
+    "Bob": {
+        "name": "Bob",
+        "vignette": "Bob is at the library. While he is reading, a woman sitting nearby gets up and asks Bob to watch her bag while she goes to get a book. Bob goes back to reading. A minute later he looks up to find the bag missing.",
+        "prompt": "What could Bob do in this situation?",
+    },
+    "Emily": {
+        "name": "Emily",
+        "vignette": "For her son’s birthday, Emily bakes a cake to serve to the children in her son’s class. One child in the class is allergic to nuts, so the cake needs to be nut-free. However, right before serving the cake, Emily realizes the cake contains nuts.",
+        "prompt": "What could Emily do in this situation?"
+    }
+} 
